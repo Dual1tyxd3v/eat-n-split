dedf93fe109cf6505a71ab8725e83851d4a8394a
B3rzxd3v <d3vilc0re@gmail.com>
2023-07-14 16:18:38 +0700

    Implemented add friend function

--- src/components/add-friend/add-friend.tsx
+++ src/components/add-friend/add-friend.tsx
@@ -1,16 +1,56 @@
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { FriendType } from '../../types/types';
 
-export default function AddFriend() {
+type AddFriendProps = {
+  addFriend: (friend: FriendType) => void;
+};
+
+export default function AddFriend({ addFriend }: AddFriendProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    image: '',
+  });
+
+  function onChangeHandler(e: ChangeEvent) {
+    const input = e.target as HTMLInputElement;
+    input.name === 'name' && setFormData({ ...formData, name: input.value });
+    input.name === 'image' && setFormData({ ...formData, image: input.value });
+  }
+
+  function onSubmitHandler(e: FormEvent) {
+    e.preventDefault();
+
+    if (!formData.name || !formData.image) {
+      return;
+    }
+
+    addFriend({ ...formData, id: new Date().getTime(), balance: 0 });
+
+    setFormData({ name: '', image: '' });
+    setIsOpen(!isOpen);
+  }
 
   return (
     <>
       {isOpen && (
-        <form className="form-add-friend">
-          <label>👫 Friend</label>
-          <input type="text" />
-          <label>🖼 Image URL</label>
-          <input type="text" />
+        <form className="form-add-friend" onSubmit={onSubmitHandler}>
+          <label htmlFor="name">👫 Friend</label>
+          <input
+            name="name"
+            id="name"
+            type="text"
+            value={formData.name}
+            onChange={onChangeHandler}
+          />
+          <label htmlFor="img">🖼 Image URL</label>
+          <input
+            name="image"
+            id="img"
+            type="text"
+            value={formData.image}
+            onChange={onChangeHandler}
+          />
           <button className="button">Add</button>
         </form>
       )}
--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,15 +3,20 @@ import { initialFriends } from '../../const';
 import FriendList from '../friend-list/friend-list';
 import AddFriend from '../add-friend/add-friend';
 import SplitBill from '../split-bill/split-bill';
+import { FriendType } from '../../types/types';
 
 function App(): JSX.Element {
-  const [friends] = useState(initialFriends);
+  const [friends, setFriends] = useState(initialFriends);
+
+  function onAddFriends(friend: FriendType) {
+    setFriends([...friends, friend]);
+  }
 
   return (
     <div className="app">
       <div className="sidebar">
         <FriendList friends={friends} />
-        <AddFriend />
+        <AddFriend addFriend={onAddFriends} />
       </div>
       <SplitBill />
     </div>
